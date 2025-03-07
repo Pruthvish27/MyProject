113cc649410351e10d083a0bf0a51bd3a07d7ebc
krishimodh <krishimodh@gmail.com>
2025-03-07 23:29:07 +0530

    add .gitignore and redirect root URL to /api/

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.shortcuts import redirect
 from rest_framework.routers import DefaultRouter
 from .views import DepartmentViewSet, DoctorViewSet, AvailabilityViewSet
 
@@ -7,6 +8,11 @@ router.register(r'department', DepartmentViewSet)  # Only Emergency
 router.register(r'doctors', DoctorViewSet)
 router.register(r'availability', AvailabilityViewSet)
 
+# Function to redirect from "/" to "/api/"
+def redirect_to_api(request):
+    return redirect('/api/')
+
 urlpatterns = [
+    path('', redirect_to_api),  # Redirect root to /api/
     path('api/', include(router.urls)),
 ]
